aed30189d5480108c4c2b838c867b2e81ee4fd2c
Ben Harris <mail@benharris.ch>
2016-09-08 12:25:41 -0400

    add meeting time

--- index.php
+++ index.php
@@ -5,8 +5,10 @@
   <title>ACM - NMU Chapter</title>
 </head>
 <body>
-Hello World
-
-Please submit a merge request for this site at the <a href="http://git.benharris.ch/acm/site">git repo</a>
+  <h1>Hello World</h1>
+  <br>
+  Please submit a merge request for this site at the <a href="http://git.benharris.ch/acm/site">git repo</a>
+  <br>
+  We meet Fridays at 4PM in the CS lab.
 </body>
 </html>
