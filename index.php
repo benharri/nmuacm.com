9d8ee522d340765e84610eff22e3fbfa6f95f453
Ben Harris <mail@benharris.ch>
2016-09-08 20:34:59 -0400

    remove <br>s

--- index.php
+++ index.php
@@ -10,13 +10,8 @@
   <p>
     Please submit a merge request for this site at the <a href="http://git.benharris.ch/acm/site">git repo</a>
   </p>
-  <br>
   <p>We meet Fridays at 4PM in the CS lab.</p>
-  <br>
   <p>Contact <a href="mailto:benharri@nmu.edu">Ben Harris</a> for more info</p>
-  <br>
-  <p>
-    <a href="http://git.benharris.ch/snippets/2">Project Ideas for this semester</a>
-  </p>
+  <p><a href="http://git.benharris.ch/snippets/2">Project Ideas for this semester</a></p>
 </body>
 </html>
