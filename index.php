d344983d79d1ba6e1b6c281e8000bbf36441b5b8
Ben Harris <mail@benharris.ch>
2016-09-08 20:42:09 -0400

    changes from brit

--- index.php
+++ index.php
@@ -2,16 +2,25 @@
 <html lang="en">
 <head>
   <meta charset="UTF-8">
+  <link rel="stylesheet" type="text/css" href="page.css">
+  <link href="https://fonts.googleapis.com/css?family=Press+Start+2P" rel="stylesheet">
   <title>ACM - NMU Chapter</title>
 </head>
 <body>
-  <h1>NMU ACM</h1>
-  <br>
-  <p>
-    Please submit a merge request for this site at the <a href="http://git.benharris.ch/acm/site">git repo</a>
-  </p>
-  <p>We meet Fridays at 4PM in the CS lab.</p>
-  <p>Contact <a href="mailto:benharri@nmu.edu">Ben Harris</a> for more info</p>
-  <p><a href="http://git.benharris.ch/snippets/2">Project Ideas for this semester</a></p>
+  <div class="main">
+    <div class="head span">
+      <div class="title">
+        NMU ACM
+      </div>
+    </div>
+    <div class="menu span">
+    </div>
+    <div class="body">
+      <p>Please submit a merge request for this site at the <a href="http://git.benharris.ch/acm/site">git repo</a></p>
+      <p>We meet Fridays at 4PM in the CS lab.</p>
+      <p>Contact <a href="mailto:benharri@nmu.edu">Ben Harris</a> for more info</p>
+      <p><a href="http://git.benharris.ch/snippets/2">Project Ideas for this semester</a>
+    </div>
+  </div>
 </body>
 </html>
