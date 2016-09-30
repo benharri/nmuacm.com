b13a0111a1126013e677360d7132fff0d8db4930
Ben Harris <mail@benharris.ch>
2016-09-30 16:35:27 -0400

    templatememe

--- memes/index.php
+++ memes/index.php
@@ -1,10 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <title>Memiez</title>
-</head>
+<?php include_once "../header.php"; ?>
 <body>
+<?php include_once "../nav.html";?>
   <img src="8050e2aa9c9b41aba06cb19763a3e06e.png" alt="meme1">
   <img src="9xYJDBP.jpg" alt="meme2">
   <img src="cbIstsV.jpg" alt="meme3">
@@ -12,4 +8,4 @@
   <img src="l2K2hfVS4R1oTA-U2CYNC6kL-WTkALkIjgHbVECIp3M.jpg" alt="hugh">
   <img src="mlrFd8P.jpg" alt="meme6">
 </body>
-</html>
+<?php include_once "footer.php";
